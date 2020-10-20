5786dca297e34b1b6277b2851be613f07113893e
Padma167 <71583423+Padma167@users.noreply.github.com>
2020-10-20 12:06:24 +0530

    10-20-2020

---
+++ src/main/java/com/xworkz/page/login/LoginPage.java
@@ -0,0 +1,10 @@
+package com.xworkz.page.login;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	public LoginPage(WebDriver driver) {
+		super();
+	}
+
+}
---
+++ src/main/java/com/xworkz/page/register/RegisterPage.java
@@ -0,0 +1,44 @@
+package com.xworkz.page.register;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class RegisterPage {
+	private WebDriver driver;
+	
+	public RegisterPage(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
+	
+	private By emailID = By.id("email");
+	private By password = By.id("psw");
+	private By repeatPassword = By.id("psw-repeat"); 
+	private By course = By.id("courses");
+	private By gender = By.id("female");
+	public void enterEmailID(String email)
+	{
+		driver.findElement(emailID).sendKeys(email);
+	}
+	
+	public void enterPassword(String pwd)
+	{
+		driver.findElement(password).sendKeys(pwd);
+	}
+	public void reenterPassword(String pwd)
+	{
+		driver.findElement(repeatPassword).sendKeys(pwd);
+	}
+	public void selectCourse(int courseindex)
+	{
+		WebElement coursesdropdown = driver.findElement(course);
+		Select select=new Select(coursesdropdown);
+		select.selectByIndex(courseindex);
+	}
+	public void selectGender()
+	{
+		driver.findElement(gender).click();
+	}
+}
